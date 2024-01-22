276d8db1ed64f9c3d9fa1b0b2b11355043140201
Ching <marcrohannmching@iskolarngbayan.pup.edu.ph>
2024-01-22 22:34:53 +0800

    Input of word and number

--- exercise 4.py
+++ exercise 4.py
@@ -1,2 +1,6 @@
 #Exercise 4: Remove first n characters from a string
 #Write a program to remove characters from a string starting from zero up to n and return a new string.
+
+word_input = input("Input Word ")
+print("your word is", word_input)
+chosen_number = int(input())
